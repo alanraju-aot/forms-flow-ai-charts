d90210e3167fb35877b25b291a10e248fa33b0a3
unknown <alan.raju@aot-technologies.com>
2026-09-22 09:40:53 +0530

    removed data-analysis

--- scripts/uninstall-forms-flow.sh
+++ scripts/uninstall-forms-flow.sh
@@ -13,8 +13,6 @@ helm uninstall forms-flow-analytics -n $NAMESPACE
 
 helm uninstall forms-flow-bpm -n $NAMESPACE
 
-helm uninstall forms-flow-data-analysis -n $NAMESPACE
-
 helm uninstall forms-flow-forms -n $NAMESPACE
 
 helm uninstall forms-flow-idm -n $NAMESPACE
